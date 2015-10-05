8437cd57b6b1689b873213ebaf85c005e04d6e5f
Rauno Kosula <raunkos@greeny.cs.tlu.ee>
2015-10-05 12:40:25 +0300

    Lisatud getAllData

--- functions.php
+++ functions.php
@@ -1,5 +1,24 @@
 <?php
-
+    //loome AB ühenduse
+    require_once("../config_global.php");
+    $database = "if15_raunkos";
+	
+	
+		function getAllData() {
+        $mysqli = new mysqli($GLOBALS["servername"], $GLOBALS["server_username"], $GLOBALS["server_password"], $GLOBALS["database"]);
+		$stmt = $mysqli->prepare("SELECT id, user_id, number_plate, color FROM car_plates");
+		$stmt->bind_result($id_from_db, $user_from_db, $number_plate_from_db, $color_from_db);
+		//Iga rea kohta teeme midagi
+		while($stmt->fetch()) {
+			//Saime andmed katte
+			echo($user_id_from_db);
+			
+		}
+		
+		
+	$stmt->close();
+	$mysqli->close();
+	}
 
 
 
